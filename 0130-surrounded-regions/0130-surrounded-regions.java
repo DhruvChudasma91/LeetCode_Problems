802a6fc57d38f8c0a66613d97a1f9b6f8f375b64
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-02 15:11:53 +0530

    0130-surrounded-regions Medium .java 01-02-2026

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,56 +1,45 @@
 class Solution {
     public void solve(char[][] board) {
 
+        if(board == null || board.length == 0) return;
+
         int m = board.length;
         int n = board[0].length;
-        boolean[][] visited = new boolean[m][n];
+        
+        for(int i = 0; i < n; i++) {
+            dfs(board, 0, i);
+            dfs(board, m - 1, i);
+        }
 
         for(int i = 0; i < m; i++) {
-
-            for(int j = 0; j < n; j++) {
-
-                if((i == 0 || j == 0 || i == m - 1 || j == n - 1) && board[i][j] == 'O') {
-
-                    bfs(i, j, board, visited, m, n);
-                }
-            }
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
         }
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
 
-                if(board[i][j] == 'O' && visited[i][j] == false) {
+                if(board[i][j] == 'O') {
                     board[i][j] = 'X';
+                } else if(board[i][j] == '#') {
+                    board[i][j] = 'O';
                 }
             }
         }  
     }
 
-    public void bfs(int i, int j, char[][] board, boolean[][] visited, int m, int n) {
+    public void dfs(char[][] board, int i, int j) {
 
-        Queue<int[]> queue = new LinkedList<>();
-        visited[i][j] = true;
-        queue.add(new int[]{i, j});
-
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-        while(!queue.isEmpty()) {
-
-            int[] cell = queue.remove();
-            int row = cell[0];
-            int col = cell[1];
-
-            for(int[] dir : directions) {
-
-                int x = row + dir[0];
-                int y = col + dir[1];
-
-                if(x >= 0 && y >= 0 && x < m && y < n && board[x][y] == 'O' && visited[x][y] == false) {
-                    visited[x][y] = true;
-                    queue.add(new int[]{x, y});
-                }
-            }
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') {
+            return;
         }
+
+        board[i][j] = '#';
+
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
 
     }
 }
