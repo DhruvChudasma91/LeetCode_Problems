51a4c420b61dd1994170161a3e51093dfcfa8584
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-05 17:49:57 +0530

    1020-number-of-enclaves Medium .java 01-05-2026

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 0; i < n; i++) {
+            dfs(grid, 0, i);
+            dfs(grid, m - 1, i);
+        }
+
+        for(int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n - 1);
+        }
+
+        int moves = 0;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                if(grid[i][j] == 1) {
+                    moves++;
+                } else if(grid[i][j] == -1) {
+                    grid[i][j] = 1;
+                } 
+            }
+        }
+
+        return moves;
+    }
+
+    public void dfs(int[][] grid, int i, int j) {
+
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != 1) {
+            return;
+        }
+
+        grid[i][j] = -1;
+
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+
+}
