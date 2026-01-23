de9cc91e17693c7de8b84c365055e62c1c502ec5
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-01-23 10:33:43 +0530

    0013-roman-to-integer Easy .java 01-23-2026

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,42 @@
-class Solution {
-    public int romanToInt(String s) {
-        int res = 0;
-        Map<Character, Integer> roman = new HashMap<>();
-        roman.put('I', 1);
-        roman.put('V', 5);
-        roman.put('X', 10);
-        roman.put('L', 50);
-        roman.put('C', 100);
-        roman.put('D', 500);
-        roman.put('M', 1000);
+import java.util.*;
 
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
-                res -= roman.get(s.charAt(i));
-            } else {
-                res += roman.get(s.charAt(i));
+class Solution {
+
+    static Map<String, Integer> values = new HashMap<>();
+
+    static {
+        values.put("I", 1);
+        values.put("V", 5);
+        values.put("X", 10);
+        values.put("L", 50);
+        values.put("C", 100);
+        values.put("D", 500);
+        values.put("M", 1000);
+        values.put("IV", 4);
+        values.put("IX", 9);
+        values.put("XL", 40);
+        values.put("XC", 90);
+        values.put("CD", 400);
+        values.put("CM", 900);
+    }
+
+    public int romanToInt(String s) {
+        int sum = 0;
+        int i = 0;
+        while (i < s.length()) {
+            if (i < s.length() - 1) {
+                String two = s.substring(i, i + 2);
+                if (values.containsKey(two)) {
+                    sum += values.get(two);
+                    i += 2;
+                    continue;
+                }
             }
+            String one = s.substring(i, i + 1);
+            sum += values.get(one);
+            i += 1;
         }
 
-        return res + roman.get(s.charAt(s.length() - 1));        
+        return sum;
     }
 }
