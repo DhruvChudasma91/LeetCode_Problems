d465768fafe01d4259cb5a287cfdcb57655ab1bf
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-03-05 14:36:02 +0530

    1094-car-pooling Medium .java 03-05-2026

---
+++ 1094-car-pooling/1094-car-pooling.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        
+        int[] stops = new int[1001];
+
+        for (int[] trip : trips) {
+            int passengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+
+            stops[from] += passengers;
+            stops[to] -= passengers;
+        }
+
+        int currentPassengers = 0;
+
+        for (int i = 0; i < 1001; i++) {
+            currentPassengers += stops[i];
+            if (currentPassengers > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
