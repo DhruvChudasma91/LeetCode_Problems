75f400e0bdd65d30d7303d0ff5a0f550d80f5d68
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-14 17:31:14 +0530

    1143-longest-common-subsequence Medium .java 06-14-2026

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,6 +1,10 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
 
+        if(text1.length() == 0 || text2.length() == 0){
+            return 0;
+        }
+
         return findLCS(text1, text2);       
         
     }
@@ -10,20 +14,24 @@ class Solution {
         int n1 = text1.length();
         int n2 = text2.length();
 
-        int[][] dp = new int[n1+1][n2+1];
+        int[] dp = new int[n2+1];
 
         for(int ind1 = 1; ind1 <= n1; ind1++) {
+
+            int[] curr = new int[n2+1];
 
             for(int ind2 = 1; ind2 <= n2; ind2++) {
 
                 if(text1.charAt(ind1 - 1) == text2.charAt(ind2 - 1)) {
-                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    curr[ind2] = 1 + dp[ind2-1];
                 } else {
-                    dp[ind1][ind2] = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                    curr[ind2] = Math.max(dp[ind2], curr[ind2-1]);
                 }
             }
+
+            dp = curr.clone();
         }
 
-        return dp[n1][n2];
+        return dp[n2];
     }
 }
