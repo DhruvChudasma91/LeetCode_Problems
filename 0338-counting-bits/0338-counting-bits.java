f65e4e311f2138e80087685bba24cee2dd6a56f4
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-14 22:38:39 +0530

    0338-counting-bits Easy .java 06-14-2026

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,21 +2,26 @@ class Solution {
     public int[] countBits(int n) {
 
         int[] ans = new int[n+1];
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
         
         for(int i = n; i>=0; i--) {
 
-            ans[i] = countOnes(i);
+            ans[i] = countOnes(i, dp);
         }
 
         return ans;
     }
 
-    public int countOnes(int n) {
+    public int countOnes(int n, int[] dp) {
 
         if(n == 0) {
             return 0;
         }
 
-        return n % 2 + countOnes(n / 2);
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = n % 2 + countOnes(n / 2, dp);
     }
 }
