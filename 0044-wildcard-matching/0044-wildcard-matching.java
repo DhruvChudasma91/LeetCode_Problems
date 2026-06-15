f1a6dbe229e04f061706921b3a72594c6cc18159
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-15 12:39:50 +0530

    0044-wildcard-matching Hard .java 06-15-2026

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -4,14 +4,49 @@ class Solution {
         int n = s.length();
         int m = p.length();
 
-        int[][] dp = new int[n][m];
-        for(int[] d : dp) {
-            Arrays.fill(d, -1);
+        boolean[][] dp = new boolean[n+1][m+1];
+        dp[0][0] = true;
+
+        for(int i = 1; i <= n; i++) {
+            dp[i][0] = false;
         }
 
-        return wcm(n-1, m-1, s, p, dp);
+        for(int j = 1; j <= m; j++) {
+            dp[0][j] = isRemainingStars(j, p);
+        }
+
+        for(int i = 1; i <= n; i++) {
+
+            for(int j = 1; j <= m; j++) {
+                
+                if(p.charAt(j - 1) == '?' || s.charAt(i-1) == p.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1]; 
+                } else if(p.charAt(j - 1) == '*'){
+                    dp[i][j] = (dp[i][j-1] || dp[i-1][j]);
+                } else {
+                    dp[i][j] = false;
+                }
+
+            }
+        }
+
+        return dp[n][m];
         
     }
+
+    public static boolean isRemainingStars(int ind, String p){
+
+        for(int i = 1; i<=ind; i++) {
+
+            if(p.charAt(i-1) != '*'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /*
 
     public boolean wcm(int ind1, int ind2, String s, String p, int[][] dp) {
 
@@ -40,15 +75,6 @@ class Solution {
         return false;
     }
 
-    public boolean isRemainingStars(int ind, String p){
+    */
 
-        for(int i = ind; i>=0; i--) {
-
-            if(p.charAt(i) != '*'){
-                return false;
-            }
-        }
-
-        return true;
-    }
 }
