f59d7a51c69f1908a6e07af16ef0066c8ea4431f
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-17 10:20:50 +0530

    0122-best-time-to-buy-and-sell-stock-ii Medium .java 06-17-2026

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,42 @@
 class Solution {
-    public int maxProfit(int[] prices) {      
-        int profitFromPriceGain = 0;
+    public int maxProfit(int[] prices) {
         
-        for( int i = 0 ; i < prices.length-1 ; i++ ){
-            
-            if( prices[i] < prices[i+1] ){
-                profitFromPriceGain += ( prices[i+1] - prices[i] );
-            }
+        int n = prices.length;
+
+        if(n == 0) return 0;
+
+        int[][] dp = new int[n][2];
+        for(int[] d : dp) {
+            Arrays.fill(d, -1);
         }
-        
-        return profitFromPriceGain;
+
+        return helper(0, 0, n, prices, dp);
+    }
+
+    public int helper(int ind, int buy, int n, int[] prices, int[][] dp){
+
+        if(ind == n) return 0;
+
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+
+        int profit = 0;
+
+        if(buy == 0) {
+
+            int skip = helper(ind + 1, buy, n, prices, dp);
+            int take = (-1 * prices[ind]) + helper(ind + 1, 1, n, prices, dp);
+
+            profit = Math.max(skip, take);
+        }
+
+        if(buy == 1){
+
+           int skip = helper(ind + 1, buy, n, prices, dp); 
+           int use = prices[ind] + helper(ind + 1, 0, n, prices, dp);
+
+           profit = Math.max(skip, use);
+        }
+
+        return dp[ind][buy] = profit;
     }
 }
