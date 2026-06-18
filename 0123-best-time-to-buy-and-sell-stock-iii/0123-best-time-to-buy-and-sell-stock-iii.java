8b1f14054373de6c7d1330984566609a0d06dcbb
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-18 11:35:12 +0530

    0123-best-time-to-buy-and-sell-stock-iii Hard .java 06-18-2026

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int n = prices.length;
+
+        int[][][] dp = new int[n][2][3];
+        for(int[][] d : dp){
+
+            for(int[] it : d) {
+                Arrays.fill(it, -1);
+            }
+           
+        }
+
+        return helpar(0, 1, 2, n, prices, dp);
+    }
+
+    public int helpar(int ind, int buy, int total, int n, int[] prices, int[][][] dp){
+
+        if(ind == n || total == 0){
+            return 0;
+        }
+
+        if(dp[ind][buy][total] != -1) {
+            return dp[ind][buy][total];
+        }
+
+        int profit = 0;
+        if(buy == 1) {
+
+            int skip = helpar(ind + 1, 1, total, n, prices, dp);
+            int take = -prices[ind] + helpar(ind + 1, 0, total, n, prices, dp);
+
+            profit = Math.max(skip, take);
+        }
+
+        if(buy == 0) {
+
+            int skip = helpar(ind + 1, 0, total, n, prices, dp);
+            int take = prices[ind] + helpar(ind + 1, 1, total - 1, n, prices, dp);
+
+            profit = Math.max(skip, take);
+        }
+
+        dp[ind][buy][total] = profit;
+
+        return profit;
+    }
+}
