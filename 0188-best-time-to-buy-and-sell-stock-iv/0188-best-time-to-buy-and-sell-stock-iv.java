1ad236bbcef293dbb6ee0c3777420c3f86ec0829
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-18 19:00:13 +0530

    0188-best-time-to-buy-and-sell-stock-iv Hard .java 06-18-2026

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -3,16 +3,35 @@ class Solution {
         
         int n = prices.length;
 
-        int[][][] dp = new int[n][2][k+1];
+        int[][][] dp = new int[n+1][2][k+1];
 
-        for(int[][] d : dp) {
-            for(int[] it : d) {
-                Arrays.fill(it, -1);
+        for(int ind = n-1; ind>=0; ind--) {
+            for(int buy = 0; buy <= 1; buy++) {
+                for(int cap = 1; cap <= k; cap++) {
+
+                    if(buy == 1) {
+
+                        int skip = dp[ind + 1][1][cap];
+                        int take = -prices[ind] + dp[ind + 1][0][cap];
+
+                        dp[ind][buy][cap] = Math.max(skip, take);
+
+                    } else {
+
+                        int skip = dp[ind + 1][0][cap];
+                        int take = prices[ind] + dp[ind + 1][1][cap - 1];
+
+                        dp[ind][buy][cap] = Math.max(skip, take);
+                    }
+
+                }
             }
         }
 
-        return helpar(0, 1, k, n, prices, dp);
+        return dp[0][1][k];
     }
+
+    /*
 
     public int helpar(int ind, int buy, int cap, int n, int[] prices, int[][][] dp) {
 
@@ -42,4 +61,6 @@ class Solution {
 
         return profit;
     }
+
+    */
 }
