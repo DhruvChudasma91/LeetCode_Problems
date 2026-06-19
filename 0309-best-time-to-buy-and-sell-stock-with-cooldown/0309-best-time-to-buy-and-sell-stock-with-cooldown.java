f456d5d0a246000f7cbec28211e815838a7e7b82
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-19 10:22:47 +0530

    0309-best-time-to-buy-and-sell-stock-with-cooldown Medium .java 06-19-2026

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,14 +3,33 @@ class Solution {
         
         int n = prices.length;
 
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n+2][2];
 
-        for(int[] d : dp) {
-            Arrays.fill(d, -1);
+        for(int ind = n-1; ind >= 0; ind--) {  //n = 5, n + 1 = 6 // n - 2 + 2 => 5 - 1 + 2 => 6
+
+            for(int buy = 0; buy <= 1; buy++) {
+
+                if(buy == 1) {
+
+                    int skip = dp[ind + 1][1];
+                    int take = -prices[ind] + dp[ind + 1][0];
+
+                    dp[ind][buy] = Math.max(skip, take);
+
+                } else {
+
+                    int skip = dp[ind + 1][0];
+                    int take = prices[ind] + dp[ind + 2][1];
+
+                    dp[ind][buy] = Math.max(skip, take);
+                }
+            }
         }
 
-        return helpar(0, 1, n, prices, dp);
+        return dp[0][1];
     }
+
+    /*
 
     public int helpar(int ind, int buy, int n, int[] prices, int[][] dp) {
 
@@ -42,5 +61,7 @@ class Solution {
         dp[ind][buy] = profit;
 
         return profit;
-    } 
+    }
+
+    */ 
 }
