676c3f2b47066c81847cb1c526bbd2a816dd55a4
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-25 17:55:41 +0530

    0312-burst-balloons Hard .java 06-25-2026

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        
+        int n = nums.length;
+        int[] arr = new int[n + 2];
+        arr[0] = 1;
+        arr[n + 1] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            arr[i] = nums[i - 1];
+        }
+
+        int[][] dp = new int[n + 2][n + 2];
+        for(int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+
+        return helpar(1, n, arr, dp);
+    }
+
+    public int helpar(int i, int j, int[] arr, int[][] dp) {
+
+        if(i > j) {
+            return 0;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int max = -(int)1e9;
+
+        for(int ind = i; ind <= j; ind++) {
+
+            int coins = arr[i - 1] * arr[ind] * arr[j + 1] + helpar(i, ind - 1, arr, dp) + helpar(ind + 1, j, arr, dp);
+            max = Math.max(max, coins);
+        }
+
+        dp[i][j] = max;
+
+        return max;
+    }
+}
