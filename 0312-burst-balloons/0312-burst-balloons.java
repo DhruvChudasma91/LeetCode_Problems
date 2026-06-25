55f0cb24ca0de483a719c06673b45eff99b15a68
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-25 18:03:30 +0530

    0312-burst-balloons Hard .java 06-25-2026

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -11,12 +11,27 @@ class Solution {
         }
 
         int[][] dp = new int[n + 2][n + 2];
-        for(int[] d : dp) {
-            Arrays.fill(d, -1);
+
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= n; j++) {
+
+                if(i > j) continue;
+                int max = -(int)1e9;
+
+                for(int ind = i; ind <= j; ind++) {
+
+                    int coins = arr[i - 1] * arr[ind] * arr[j + 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                    max = Math.max(max, coins);
+                }
+
+                dp[i][j] = max;
+            }
         }
 
-        return helpar(1, n, arr, dp);
+        return dp[1][n];
     }
+
+    /*
 
     public int helpar(int i, int j, int[] arr, int[][] dp) {
 
@@ -38,4 +53,6 @@ class Solution {
 
         return max;
     }
+
+    */
 }
