d72b526adff8528d5d78cd23a35488aca02b84e3
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-26 12:46:01 +0530

    1043-partition-array-for-maximum-sum Medium .java 06-26-2026

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -3,11 +3,32 @@ class Solution {
         
         int n = arr.length;
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        int[] dp = new int[n+1];
+        
+        for(int first = n - 1; first >= 0; first--) {
 
-        return helpar(0, n, k, arr, dp);
+            int maxSum = Integer.MIN_VALUE;
+            int maxNum = Integer.MIN_VALUE;
+            int len = 0;
+
+            for(int last = first; last < Math.min(first + k, n); last++) {
+
+                len++;
+                maxNum = Math.max(maxNum, arr[last]);
+
+                int sum = (len * maxNum) + dp[last + 1];
+
+                maxSum = Math.max(sum, maxSum);
+            }
+
+            dp[first] = maxSum;
+
+        }
+
+        return dp[0];
     }
+
+    /*
 
     public int helpar(int first, int n, int k, int[] arr, int[] dp) {
 
@@ -31,4 +52,6 @@ class Solution {
 
         return dp[first] = maxSum;
     }
+
+    */
 }
