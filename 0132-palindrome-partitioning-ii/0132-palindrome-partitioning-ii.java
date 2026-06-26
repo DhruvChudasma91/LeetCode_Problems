5e73774971d065ca33c839a14a4241d63598311e
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-06-26 12:09:39 +0530

    0132-palindrome-partitioning-ii Hard .java 06-26-2026

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.java
@@ -3,33 +3,48 @@ class Solution {
         
         int n = s.length();
 
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
+        int[] dp = new int[n+1];
+        dp[n] = -1;
 
-        return helpar(0, s, dp);
+        for(int f = n - 1; f >= 0; f--) {
+
+            int min = (int)1e9;
+            for(int last = f; last < n; last++) {
+
+                if(isPalindrome(f, last, s)) {
+                    
+                    int cut = 1 + dp[last + 1];
+                    min = Math.min(min, cut);
+                } 
+            }
+            
+            dp[f] = min;
+        }
+
+        return dp[0];
     }
 
-    public int helpar(int first, String s, int[] dp) {
+    // public int helpar(int first, String s, int[] dp) {
         
-        if(first == s.length() || isPalindrome(first, s.length() - 1, s)) {
-            return 0;
-        }
+    //     if(first == s.length() || isPalindrome(first, s.length() - 1, s)) {
+    //         return 0;
+    //     }
 
-        if(dp[first] != -1) return dp[first];
+    //     if(dp[first] != -1) return dp[first];
 
-        int min = (int)1e9;
+    //     int min = (int)1e9;
 
-        for(int last = first; last < s.length(); last++) {
+    //     for(int last = first; last < s.length(); last++) {
 
-            if(isPalindrome(first, last, s)) {
+    //         if(isPalindrome(first, last, s)) {
                 
-                int cut = 1 + helpar(last + 1, s, dp);
-                min = Math.min(min, cut);
-            } 
-        }
+    //             int cut = 1 + helpar(last + 1, s, dp);
+    //             min = Math.min(min, cut);
+    //         } 
+    //     }
 
-        return dp[first] = min;
-    }
+    //     return dp[first] = min;
+    // }
 
     public boolean isPalindrome(int l, int r, String s) {
 
