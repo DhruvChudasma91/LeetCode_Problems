da9a48fbc77a4f812471c6dadc3ed12e070a1884
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-03 21:26:41 +0530

    0343-integer-break Medium .java 07-03-2026

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -7,15 +7,14 @@ class Solution {
             return 2;
         }
 
-        // Try to divide n into as many threes as possible
         int threes = n / 3;
         int remainder = n % 3;
 
         if (remainder == 1) {
-            threes -= 1; // remove 3 * 1
-            remainder = 4; // create 2 * 2
+            threes -= 1; 
+            remainder = 4; 
         } else if (remainder == 0) {
-            remainder = 1; // when remainder is 0, set 1 which doesn't affect your answer.
+            remainder = 1;
         }
 
         return (int) (Math.pow(3, threes) * remainder);        
