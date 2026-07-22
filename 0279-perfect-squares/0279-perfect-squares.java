7fabec677a3ef908b811923580b553920cea4e0b
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-22 12:23:07 +0530

    0279-perfect-squares Medium .java 07-22-2026

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -3,14 +3,46 @@ class Solution {
 
         ArrayList<Integer> list = findPerSqrt(n);
         int len = list.size();
-        int[][] dp = new int[len][n+1];
 
-        for(int[] d : dp) {
-            Arrays.fill(d, -1);
+        int[][] dp = new int[len][n+1];
+        
+        for (int target = 0; target <= n; target++) {
+            if (target % list.get(0) == 0) {
+                dp[0][target] = target / list.get(0);
+            } else {
+                dp[0][target] = (int) 1e9;
+            }
         }
 
-        return helpar(len - 1, n, list, dp);
+        for (int ind = 1; ind < len; ind++) {
+            for (int target = 0; target <= n; target++) {
+
+                int notPick = dp[ind - 1][target];
+
+                int pick = (int) 1e9;
+                if (target >= list.get(ind)) {
+                    pick = 1 + dp[ind][target - list.get(ind)];
+                }
+
+                dp[ind][target] = Math.min(notPick, pick);
+            }
+        }
+
+        return dp[len-1][n];
     }
+
+    public ArrayList<Integer> findPerSqrt(int n) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 1; i * i <= n; i++) {
+            list.add(i * i);
+        }
+
+        return list;
+    }
+
+    /*
 
     public int helpar(int ind, int target, ArrayList<Integer> list, int[][] dp) {
 
@@ -39,14 +71,5 @@ class Solution {
         return dp[ind][target] = Math.min(skip, pick);
     }
 
-    public ArrayList<Integer> findPerSqrt(int n) {
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for(int i = 1; i * i <= n; i++) {
-            list.add(i * i);
-        }
-
-        return list;
-    }
+    */
 }
