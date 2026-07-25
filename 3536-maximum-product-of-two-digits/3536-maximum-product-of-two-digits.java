b5e73479f8979b648b75b50ca5766bacf8a5d21f
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-25 15:49:01 +0530

    3536-maximum-product-of-two-digits Easy .java 07-25-2026

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProduct(int n) {
+
+        if(n == 10) return 0;
+        
+        int num = n;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        while(num > 0) {
+
+            int digit = num % 10;
+
+            if(digit > max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if(digit > max2) {
+                max2 = digit;
+            }
+
+            num /= 10;
+        }
+
+        return max1 * max2;
+    }
+}
