0c16f82e670d333696bd7575d20134363d7557d8
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-26 09:58:21 +0530

    0628-maximum-product-of-three-numbers Easy .java 07-26-2026

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,44 +1,34 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        
-        int n = nums.length;
 
-        int max1 = -1000;
-        int max2 = -1000;
-        int max3 = -1000;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
 
         int min1 = Integer.MAX_VALUE;
         int min2 = Integer.MAX_VALUE;
 
-        for(int num : nums) {
+        for (int num : nums) {
 
-            if(num > max1) {
+            if (num >= max1) {
                 max3 = max2;
                 max2 = max1;
                 max1 = num;
-            } else if(num > max2) {
+            } else if (num >= max2) {
                 max3 = max2;
                 max2 = num;
-            } else if(num > max3) {
+            } else if (num > max3) {
                 max3 = num;
             }
 
-            if(num < 0 && min1 > num) {
+            if (num <= min1) {
                 min2 = min1;
                 min1 = num;
-            } else if(num < 0 && min2 > num) {
+            } else if (num < min2) {
                 min2 = num;
             }
-
-            System.out.println(num);
-            System.out.println("max1: " + max1 + " max2: " + max2 + " max3: " + max3);
-            System.out.println("min1: " + min1 + " min2: " + min2);
         }
 
-        if(min1 != Integer.MAX_VALUE && min2 != Integer.MAX_VALUE) {
-            return Math.max(max1 * max2 * max3, min1 * min2 * max1);
-        }
-
-        return max1 * max2 * max3;
+        return Math.max(max1 * max2 * max3, max1 * min1 * min2);
     }
 }
