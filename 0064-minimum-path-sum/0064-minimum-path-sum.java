6460214d2d27201926e7eaa056bbea340a78d950
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-29 11:52:25 +0530

    0064-minimum-path-sum Medium .java 07-29-2026

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,6 +1,48 @@
 class Solution {
     public int minPathSum(int[][] grid) {
         
+        //Space Optimization
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int[] prev = new int[m];
+        prev[0] = grid[0][0];
+
+        for(int row = 0; row < n; row++) {
+
+            int[] curr = new int[m];
+            for(int col = 0; col < m; col++) {
+
+                if(row == 0 && col == 0) {
+                    curr[0] = grid[0][0];
+                    continue;
+                }
+
+                int left = (int)1e9;
+                int up = (int)1e9;
+
+                if(col > 0) {
+                    left = grid[row][col] + curr[col-1];
+                }
+
+                if(row > 0) {
+                    up = grid[row][col] + prev[col];
+                }
+
+                curr[col] = Math.min(left, up);
+            }
+
+            prev = curr.clone();
+        }
+        
+        return prev[m-1];
+    }
+
+
+    /*
+
+    public int tebulation(int[][] grid) {
+    
         int n = grid.length;
         int m = grid[0].length;
 
@@ -29,9 +71,9 @@ class Solution {
         }
         
         return dp[n-1][m-1];
+    
+    
     }
-
-    /*
 
     public int helper(int row, int col, int[][] grid, int[][] dp) {
 
