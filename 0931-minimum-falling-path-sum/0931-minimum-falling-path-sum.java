afc967fa7578a7130fea1b0ceeedf723e099a050
Dhruv Chudasma <156397151+DhruvChudasma91@users.noreply.github.com>
2026-07-29 13:09:23 +0530

    0931-minimum-falling-path-sum Medium .java 07-29-2026

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,22 +1,50 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        
-        int n = matrix.length;
-        int m = matrix[0].length;
 
-        Integer[][] dp = new Integer[n][m];
+        return tebulation(matrix);
+    }
+
+    public int tebulation(int[][] matrix) {
+
+        int n = matrix.length;
+
+        int[][] dp = new int[n][n];
+
+        for(int i = 0; i < n; i++) {
+            dp[n-1][i] = matrix[n-1][i];
+        }
+
+        for(int row = n-2; row >= 0; row--) {
+            for(int col = 0; col < n; col++) {
+
+                    int below = dp[row+1][col];
+                    int lDiago = (int)1e9;
+                    int rDiago = (int)1e9;
+
+                    if(col > 0) {
+                        lDiago = dp[row+1][col-1];
+                    }
+
+                    if(col < n -1) {
+                        rDiago = dp[row+1][col+1];
+                    }
+
+                    dp[row][col] = matrix[row][col] + Math.min(below, Math.min(lDiago, rDiago));
+
+            }
+        }
 
         int ans = (int)1e9;
-        for(int i = 0; i < m; i++) {
-            int sum = helper(0, i, n, m, matrix, dp);
-
-            ans = Math.min(sum, ans);
+        for(int i = 0; i < n; i++) {
+            ans = Math.min(ans, dp[0][i]);
         }
 
         return ans;
     }
 
-    public int helper(int row, int col, int n, int m, int[][] matrix, Integer[][] dp) {
+    /*
+
+    public int memoization(int row, int col, int n, int m, int[][] matrix, Integer[][] dp) {
 
         if(row >= n || col >= m || col < 0) {
             return (int)1e9;
@@ -36,4 +64,6 @@ class Solution {
 
         return dp[row][col] = Math.min(below, Math.min(lDiago, rDiago));
     }
+
+    */
 }
